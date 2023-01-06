9886855db84a16dccffa1438693e6cfdfc523690
KevinAddae <kevinaddae94@gmail.com>
2023-01-06 18:01:43 +0000

    Created the board activity_tic_tac_toe_board.xml where the actual game will take place

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tictactoeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun singleBtnEvent(view: View) {
+        var i = Intent(this, TicTacToeBoard::class.java)
+        startActivity(i)
     }
 }
---
+++ app/src/main/java/com/example/tictactoeapp/TicTacToeBoard.kt
@@ -0,0 +1,11 @@
+package com.example.tictactoeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TicTacToeBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe_board)
+    }
+}
